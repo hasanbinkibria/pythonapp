4cf9acb14fc92b2df97ec67aec2089f3148bdb60
hasanbinkibria <hasanbinkibria@gmail.com>
2023-08-17 22:04:18 +0600

    dskjfh

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
-print("Hello, World!")
 
-print("Hello Double World")
+class multiple():
+    def __init__(self, number1, number2):
+        self.f = number1
+        self.s = number2
+
+        
+number = multiple(232,32)
+print(number.f)
+print(number.s)
