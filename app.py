20d1778260bf13885d3fa07a2426afd6aa120a10
hasanbinkibria <hasanbinkibria@gmail.com>
2023-08-23 00:41:04 -0700

    Done So Many Things

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 from functions import function1
+from clases import user
 result = function1(34)
 
 print(f"Hello {result}")
+inputusername = input("Enter Your Name : ")
+inputage = int(input("Enter Your Age : "))
+person = user(f"{inputusername}",f"{inputage}")
+
+print(f"Your name is",person.name)
+print(f"Your age is",person.age)
